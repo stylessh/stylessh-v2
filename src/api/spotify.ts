98b5526eea1cbf7cf98f38ed769bdcfc8f2b3ba6
stylessh <stylessh@gmail.com>
2022-08-04 13:30:26 -0400

    feat: spotify current listening added

---
+++ src/api/spotify.ts
@@ -0,0 +1,40 @@
+const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+
+const basic = btoa(
+  `${import.meta.env.SPOTIFY_CLIENT_ID}:${
+    import.meta.env.SPOTIFY_CLIENT_SECRET
+  }`
+);
+
+const getAccessToken = async () => {
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${basic}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      grant_type: "refresh_token",
+      refresh_token: import.meta.env.SPOTIFY_REFRESH_TOKEN,
+    }).toString(),
+  });
+
+  return response.json();
+};
+
+export default async () => {
+  const { access_token } = await getAccessToken();
+
+  const res = await fetch(NOW_PLAYING_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+
+  if (res.status !== 200) {
+    return null;
+  }
+
+  return await res.json();
+};
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,9 @@
 interface ImportMetaEnv {
   readonly GITHUB_TOKEN: string;
+  readonly SPOTIFY_CLIENT_ID: string;
+  readonly SPOTIFY_CLIENT_SECRET: string;
+  readonly SPOTIFY_REFRESH_TOKEN: string;
+  readonly SPOTIFY_CODE: string;
 }
 
 interface ImportMeta {
