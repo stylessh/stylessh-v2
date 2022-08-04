56cd05c9d0cd773230057699115a5c61c9592ada
stylessh <stylessh@gmail.com>
2022-08-04 12:09:26 -0400

    feat: inertial scroll added, media compression added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
 
+import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind(), compress()],
+
+  experimental: {
+    integrations: true,
+  },
 });
