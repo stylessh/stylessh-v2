f9cc8dbdeca44888c2adc1fe08116d96fb7d7087
stylessh <stylessh@gmail.com>
2022-09-22 00:03:26 -0400

    feat: smooth scroll added, seo improved, repository section temporary removed

--- src/api/repositories.ts
+++ src/api/repositories.ts
@@ -1,12 +1,7 @@
 export const getRepositories = async (): Promise<any> => {
   const data = await fetch(
-    "https://api.github.com/users/stylessh/repos?per_page=50",
-    {
-      headers: {
-        Authorization: `token ${import.meta.env.GITHUB_TOKEN}`,
-      },
-    }
+    "https://api.github.com/users/stylessh/repos?per_page=50"
   ).then((res) => res.json());
 
-  return data;
+  return [];
 };
