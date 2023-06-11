9dbd53e5fdca1f49c7743c2d4798f71f451fcb15
stylessh <stylesshjs@gmail.com>
2023-06-11 12:37:45 -0400

    feat: upgrade astro to v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind(), compress({
+  integrations: [preact(), compress({
     img: false
-  })],
-  experimental: {
-    integrations: true
-  },
+  }), tailwind()]
 });
